6fbcbc533b749e50b996c140327362bf5a686a2a
johnericward <johnericward@gmail.com>
2019-10-13 21:20:09 -0400

    building out portfolio page with links

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(".appleii").mouseover(function () {
+    $(".codeTextHover").text("<CODE_PORTFOLIO>");
+});
+$(".appleii").mouseout(function () {
+    $(".codeTextHover").text("");
+});
+
+// $("p").mouseover(function(){
+//     $("p").css("background-color", "yellow");
+//   });
+
+
