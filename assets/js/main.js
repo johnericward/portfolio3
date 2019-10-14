87398268806db0d5b64767f48f207a8bdaf85f36
johnericward <johnericward@gmail.com>
2019-10-13 22:33:39 -0400

    added build section redirecting to old portfolio for now

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,30 @@ $(".appleii").mouseout(function () {
     $(".codeTextHover").text("");
 });
 
-// $("p").mouseover(function(){
-//     $("p").css("background-color", "yellow");
-//   });
+
+$(".mePic").mouseover(function () {
+    $(".mePicHover").text("<E Ward>");
+});
+$(".mePic").mouseout(function () {
+    $(".mePicHover").text("");
+});
+
+
+$(".saw").mouseover(function () {
+    $(".sawTextHover").text("<_BUILD>");
+});
+$(".saw").mouseout(function () {
+    $(".sawTextHover").text("");
+});
+
+
+$(".meCartoon").mouseover(function () {
+    $(".aboutTextHover").text("<_ABOUT>");
+});
+$(".meCartoon").mouseout(function () {
+    $(".aboutTextHover").text("");
+});
+
+
 
 
